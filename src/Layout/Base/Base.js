b19963e8c3dcb70209236fdc6fd6c6b9ed7f3796
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-04-20 14:33:40 +0600

    layout added

---
+++ src/Layout/Base/Base.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+function Base(props) {
+  return (
+    <div id="Base">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default Base;
---
+++ src/Layout/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <header id="header">
+            I am header
+        </header>
+    );
+}
+
+export default Header;
