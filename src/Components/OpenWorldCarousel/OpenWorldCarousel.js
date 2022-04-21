d0bb8cbb7d9e097735f4b05f88c708f775a717d9
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-04-21 12:22:49 +0600

    openworld page done

--- src/Components/OpenWorldCarousel/OpenWorldCarousel.js
+++ src/Components/OpenWorldCarousel/OpenWorldCarousel.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./OpenWorldCarousel.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
 
 function OpenWorldCarousel(props) {
   return (
     <div id="OpenWorldCarousel">
       <div className="container">
-        <div className="row align-items-end align-items-md-center">
-          <div className="text-center col-md-6">
-            <div className="col_wrapper content_box_img">
-              <img src="" alt="img" />
+        <div className="row">
+          <div className="col-12">
+            <div className="swiper_box_wrapper position-relative">
+              <SwiperSlideCarousel></SwiperSlideCarousel>
+              <div className="swiper_fixed_content">
+                <h2>Open World</h2>
+                <p className="text-white">
+                  Adventure on this vast world, with more than 8 continents on
+                  the Mooniverse waiting for you to explore.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="text-center text-md-start col-md-6">
-            <h3 className="text_secondary font-weight-bolder font_4">
-              Open World
-            </h3>
-            <p className="text-white">
-              Adventure on this vast world, with more than 8 continents on the
-              Mooniverse waiting for you to explore.
-            </p>
           </div>
         </div>
       </div>
@@ -26,3 +36,101 @@ function OpenWorldCarousel(props) {
 }
 
 export default OpenWorldCarousel;
+
+const SwiperSlideCarousel = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  console.log(slideIndex);
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        onSlideChange={(e) => setSlideIndex(e.realIndex)}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className={
+          slideIndex === 0
+            ? "start_point_swiper"
+            : "" || slideIndex === 7
+            ? "end_point_swiper"
+            : ""
+        }
+      >
+        <SwiperSlide>
+          <img
+            className="slideImg"
+            src={require("../../Static/img/open-world-1.91b7fc9.png")}
+            alt=""
+          />
+          <div className="swiper_description">The Mid World</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="slideImg"
+            src={require("../../Static/img/open-world-2.1092f48.png")}
+            alt=""
+          />
+          <div className="swiper_description">Ocean Pearl</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="slideImg"
+            src={require("../../Static/img/open-world-3.050c985.png")}
+            alt=""
+          />
+          <div className="swiper_description">Angel Eye</div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            className="slideImg"
+            src={require("../../Static/img/open-world-4.51b3ebd.png")}
+            alt=""
+          />
+          <div className="swiper_description">The Lost Gate</div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          {" "}
+          <img
+            className="slideImg"
+            src={require("../../Static/img/open-world-5.8be8280.png")}
+            alt=""
+          />
+          <div className="swiper_description">Good Hope</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <img
+            className="slideImg"
+            src={require("../../Static/img/open-world-6.39949c9.png")}
+            alt=""
+          />
+          <div className="swiper_description">Ersa Guardian</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <img
+            className="slideImg"
+            src={require("../../Static/img/open-world-7.01ad987.png")}
+            alt=""
+          />
+          <div className="swiper_description">New Horizon</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <img
+            className="slideImg"
+            src={require("../../Static/img/open-world-8.a2a2eef.png")}
+            alt=""
+          />
+          <div className="swiper_description">Hell Gate</div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
